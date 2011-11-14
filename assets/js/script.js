a5442bc1cdbd84a03f7b9f5d24f1053d46c0bbff
Omar J <omar.jalalzada@moovweb.com>
2011-11-13 21:47:34 -0800

    new batch of changes are here

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,16 @@
     console.log("cabs sa here");
   });
 */
+
+// Auto Scroll the page
+x$(window).on( 'load', function(){
+  setTimeout(function(){
+   if (window.pageYOffset < 50) {
+    window.scrollTo(0,1);}
+  },0);
+});
+
+// Contact Me Button
 x$(document).on("DOMContentLoaded", function() {
   x$("#contact_me").each(function(btn){
     x$(btn).on('click', function(){
@@ -13,36 +23,77 @@ x$(document).on("DOMContentLoaded", function() {
     });
   });
   
-  x$("#bg_module").on('click', function(){
-    x$("#contact_me_container").removeClass("active")
-  })
+  x$("#bg_module").each(function(module){
+    var bHeight = document.body.offsetHeight + 'px'
+    x$(module).setStyle('height', bHeight);
+    x$(module).on('click', function(){
+      x$("#contact_me_container").removeClass("active")
+    });  
+  });
 });
 
 /* Mouse over action for the display boxes */
 x$(document).on("DOMContentLoaded", function() {
-  
   x$(".box").each(function(box){
-    console.log(box)
-    x$(box).on('mouseover', function(){
-      x$(this).addClass('over')
+    x$(box).on('mouseover', function (){
+      x$(box).addClass('over');
+/*
+      function timeMsg() {
+        var t = setTimeout( function overMouse() {
+          x$(box).addClass('over');
+        },1000); 
+      };    
+     return timeMsg();
+*/
     });
+    
     x$(box).on('mouseout', function(){
       x$(this).removeClass('over')
     });
+    
   });
-  
 });
 
 /* Late loading the images for faster page loads */
 x$(document).on("DOMContentLoaded", function() {
 
+
+  // Home page
   x$('body.home_page').each(function () {
-   x$(".box").each(function(){
-    x$(this).find(".lateload").each(function(pics){
-      x$(pics).addClass("pic");
-    });
+    x$(".box").each(function(boxes){
+   
+      x$(boxes).find(".lateload").each(function(pics){
+        x$(pics).addClass("pic");
+      });
+   
+/*
+      function timeLoad() {
+        var t = setInterval( function overMouse() {
+ 
+          x$(boxes).find(".lateload").each(function(pics){
+            x$(pics).addClass("pic");
+          });
+ 
+          console.log("cabsa here!");
+        },5000); 
+      }; 
+
+    return timeLoad();
+*/
+  
    });
   });
+  // Ending Homepage 
+  
+  x$('body.project').each(function () {
+    x$(".img").each(function(boxes, index){
+      x$(boxes).find(".lateload").each(function(pics){
+        x$(pics).addClass("pic");
+      });
+   });
+  });
+
+  
   
 });
 
